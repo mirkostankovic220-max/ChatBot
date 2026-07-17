ce5b1e158c7e32186066ea80faa714f56d5ed598
mirkostankovic220-max <240983490+mirkostankovic220-max@users.noreply.github.com>
2026-07-18 02:15:32 +0300

    Update apiKey in Config.js

--- Private/Config.js
+++ Private/Config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    apiKey: "",
+    apiKey: "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",
     model: "openai/gpt-oss-20b:free",
     apiUrl: "https://openrouter.ai/api/v1/chat/completions",
 
